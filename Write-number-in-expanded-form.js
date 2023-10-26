d69a120c59979290c14c8dc9dffae7427473d80c
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-26 13:06:13 -0700

    Update Write-number-in-expanded-form.js

--- Write-number-in-expanded-form.js
+++ Write-number-in-expanded-form.js
@@ -11,3 +11,11 @@ NOTE: All numbers will be whole numbers greater than 0.
 
 
 // Solution
+
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map((a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
